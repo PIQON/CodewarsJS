38b95c5e48e78bb6bd3d4ea8cab07cd7f6912f5c
PIQON <naskret.kamil@wp.pl>
2023-03-27 20:44:09 +0200

    8kyu challenge century solution

---
+++ src/8kyu/century.js
@@ -0,0 +1,4 @@
+function century(year) {
+  // Finish this :)
+  return year % 100 > 0 ? year / 100 + 1 : year / 100;
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import { longest } from "./7kyu/longest";
+import { findEvenIndex } from "./6kyu/findEvenIndex";
 
-longest("aretheyhere", "yestheyarehere");
+findEvenIndex([1, 2, 3, 4, 3, 2, 1]);
+findEvenIndex([1, 2, 3, 4]);
+// findEvenIndex([1, 100, 50, -51, 1, 1]);
